b9e6a83167464a3247ca005780c4ed518bb00f08
NARUTO9556 <gruver5263@gmail.com>
2023-06-16 16:47:04 +0800

    ООП-2

--- src/Bicycle.java
+++ src/Bicycle.java
@@ -1,2 +1,17 @@
-public class Bicycle {
+public class Bicycle extends Transport{
+    public Bicycle(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+    @Override
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+
+    @Override
+    public void checkEngine() {
+    }
+
+    @Override
+    public void checkTrailer() {
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+        Transport car = new Car("car", 4);
+        Transport car2 = new Car("car2", 4);
+        Transport truck = new Truck("truck", 6);
+        Transport truck2 = new Truck("truck", 8);
+        Transport bicycle = new Bicycle("bicycle", 2);
+        Transport bicycle2 = new Bicycle("bicycle2", 2);
+
+
+        ServiceStation station = new ServiceStation();
+        station.check(car, null, null);
+        station.check(car2, null, null);
+        station.check(null, bicycle, null);
+        station.check(null, bicycle2, null);
+        station.check(null, null, truck);
+        station.check(null, null, truck2);
     }
 }
